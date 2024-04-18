ed69e717ad6fc1a45d509e3aa4d23aa1cba8c09b
KaioValls <rm94685@fiap.com.br>
2024-04-18 19:51:55 -0300

    Ajuda o gu2

--- src/app/components/cliente/cliente.component.ts
+++ src/app/components/cliente/cliente.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ClienteService } from '../../services/cliente.service';
 import { Cliente } from '../../interfaces/Cliente';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -17,8 +17,32 @@ export class ClienteComponent {
   clientForm : FormGroup = new FormGroup({})
 
    constructor(private clienteService:ClienteService, private formBuilder : FormBuilder) {
-    this.clientForm = this.formBuilder
+    this.clientForm = this.formBuilder.group({
+      nome : ['', Validators.required]
+    })
+  }
 
+  inserir(){
+    if(this.clientForm.valid){
+      const clientNovo: Cliente = {
+        nome: this.clientForm.value.nome,
+        telefone : this.clientForm.value.telefone,
+        id : this.generateRandomString(6)
+      }
+      this.clientForm.reset()
+      this.clienteService.adicionar(clientNovo)
+    }
+
+  }
+
+  generateRandomString(length: number): string  {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
   }
 
   listar():void{
